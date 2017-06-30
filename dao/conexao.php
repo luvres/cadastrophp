a0260d99b207f0cffcbb37e9471f0590879540ea
luvres <luvres@hotmail.com>
2017-06-29 23:13:18 -0300

    PHP Mailer

--- dao/conexao.php
+++ dao/conexao.php
@@ -1,14 +1,22 @@
 <?php
 class Conexao {
+    private static $dbtype = "mysql";
     private static $host = "mariadb-host";
     private static $usuario = "root";
-    private static $senha = "maria";        
+    private static $senha = "maria";
     private static $banco = "db_usuarios";
     private static $conexao = null;
 
+    // private static $dbtype = "pgsql";
+    // private static $host = "postgres-host";
+    // private static $usuario = "postgres";
+    // private static $senha = "postgres";
+    // private static $banco = "db_usuarios";
+    // private static $conexao = null;
+
     public static function getConexao(){
         if(empty($conexao)){
-            self::$conexao = new PDO("mysql:host=".self::$host.";dbname=".self::$banco, self::$usuario, self::$senha);
+            self::$conexao = new PDO(self::$dbtype.":host=".self::$host.";dbname=".self::$banco, self::$usuario, self::$senha);
         }
 
         return self::$conexao;
